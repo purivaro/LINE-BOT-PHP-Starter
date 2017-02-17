fb83a7847a45ebdde36f7bc7e1f5e5f04a4e4e3b
Anavach Purivaro <aun.puri@gmail.com>
2017-02-17 23:18:50 +0700

    get reply text

--- bot.php
+++ bot.php
@@ -1,16 +1,51 @@
 <?php
 $access_token = 'WNFbXbJCTv3AWv2AFQIKhe6EtJjVPzA4aSA1O1u+CtZaa0hqHH63PK+IyU+hAz3mOnt/yg6FCD62fmsf3uYkxuRZFsJ9P/p79/g8UDzPYhZzXc8lrqc2XWL4443q3d2tBaLOz1EmS3HCY0EFEu7KDgdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+// Get POST body content
+$content = file_get_contents('php://input');
 
-$headers = ['Authorization: Bearer ' . $access_token];
+// Parse JSON
+$events = json_decode($content,true);
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+// Validate parsed JSON date_add
+if(!is_null($events['events'])){
+    foreach($events['events'] as $event){
+        if($event['type']=='message' && $event['message']['type']=='text'){
+            // Get text sent
+            $text = $event['message']['text'];
 
-echo $result;
+            // Get reply token
+            $replyToken = $event['replyToken'];
+
+            // Build message to reply back
+            $message = [
+                'type' => 'text',
+                'text' => $text
+            ];
+        }
+    }
+
+    $url = 'https://api.line.me/v1/oauth/verify';
+    $data = [
+        'replyToken'=> $replyToken,
+        'message'=>[$message]
+    ];
+    $post = json_encode($data);
+    $headers = ['Content-Type: application/json','Authorization: Bearer ' . $access_token];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST")
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFILEDS, $post);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    echo $result."\r\n";
+
+}
+
+echo "OK";
+
 ?>
