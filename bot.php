e23754c1ad57f076f18abff3be7b20805093c86d
Anavach Purivaro <aun.puri@gmail.com>
2017-02-18 01:30:47 +0700

    translate

--- bot.php
+++ bot.php
@@ -14,6 +14,32 @@ if(!is_null($events['events'])){
             // Get text sent
             $text = $event['message']['text'];
 
+            $url_ibs = "http://www.ibsone.com/project/linebot/puribot/api/translate.php";
+            $text = 'ลิง';
+            $lang = 'สเปน';
+
+            $data_ibs = [
+            'text'=> $text,
+            'lang'=>$lang
+            ];
+
+            $post_ibs = json_encode($data_ibs);
+            $headers_ibs = ['Content-Type: application/json'];
+            $ch_ibs = curl_init($url_ibs);
+            curl_setopt($ch_ibs, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch_ibs, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch_ibs, CURLOPT_POSTFIELDS, $post_ibs);
+            curl_setopt($ch_ibs, CURLOPT_HTTPHEADER, $headers_ibs);
+            curl_setopt($ch_ibs, CURLOPT_FOLLOWLOCATION, 1);
+            $result_ibs = curl_exec($ch_ibs);
+            curl_close($ch_ibs);
+
+            $res = json_decode($result_ibs,true);
+
+            $feedback_txt = $res['result'];
+
+
+
             // Get reply token
             $replyToken = $event['replyToken'];
 
@@ -25,7 +51,7 @@ if(!is_null($events['events'])){
                 ],
                 [
                     'type' => 'text',
-                    'text' => "เมื่อกี้คุณพูดว่า..".$text
+                    'text' => "เมื่อกี้คุณพูดว่า..".$feedback_txt
                 ],
                 [
                     'type' => 'text',
