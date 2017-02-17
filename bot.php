1e446cce129b3e747484da02d40e8ea6d9989c55
Anavach Purivaro <aun.puri@gmail.com>
2017-02-17 23:28:00 +0700

    bf

--- bot.php
+++ bot.php
@@ -3,49 +3,46 @@ $access_token = 'WNFbXbJCTv3AWv2AFQIKhe6EtJjVPzA4aSA1O1u+CtZaa0hqHH63PK+IyU+hAz3
 
 // Get POST body content
 $content = file_get_contents('php://input');
-
 // Parse JSON
-$events = json_decode($content,true);
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-// Validate parsed JSON date_add
-if(!is_null($events['events'])){
-    foreach($events['events'] as $event){
-        if($event['type']=='message' && $event['message']['type']=='text'){
-            // Get text sent
-            $text = $event['message']['text'];
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
-            // Get reply token
-            $replyToken = $event['replyToken'];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-            // Build message to reply back
-            $message = [
-                'type' => 'text',
-                'text' => $text
-            ];
-        }
-    }
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
-    $url = 'https://api.line.me/v1/oauth/verify';
-    $data = [
-        'replyToken'=> $replyToken,
-        'message'=>[$message]
-    ];
-    $post = json_encode($data);
-    $headers = ['Content-Type: application/json','Authorization: Bearer ' . $access_token];
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFILEDS, $post);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-    echo $result."\r\n";
-
+			echo $result . "\r\n";
+		}
+	}
 }
-
 echo "OK";
-
 ?>
