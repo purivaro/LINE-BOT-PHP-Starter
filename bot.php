3ecdf7066d52b5e9a5c2f33e216e8369f8d933f0
Anavach Purivaro <aun.puri@gmail.com>
2017-02-17 22:51:06 +0700

    use token

--- bot.php
+++ bot.php
@@ -1,3 +1,16 @@
 <?php
-echo "Hello everyone, I am PuriBot!!!";
+$access_token = 'WNFbXbJCTv3AWv2AFQIKhe6EtJjVPzA4aSA1O1u+CtZaa0hqHH63PK+IyU+hAz3mOnt/yg6FCD62fmsf3uYkxuRZFsJ9P/p79/g8UDzPYhZzXc8lrqc2XWL4443q3d2tBaLOz1EmS3HCY0EFEu7KDgdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = ['Authorization: Bearer ' . $access_token];
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
 ?>
