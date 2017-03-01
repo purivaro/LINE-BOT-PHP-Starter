667e9f9a3ab4d68f76e17615163a1e5c61774c33
Anavach Purivaro <aun.puri@gmail.com>
2017-03-01 16:42:08 +0700

    s

--- puridev.php
+++ puridev.php
@@ -7,15 +7,10 @@ require __DIR__."/vendor/autoload.php";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]);
 
-
 $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$body = file_get_contents("php://input");
-try {
-  $events = $bot->parseEventRequest($body, $signature);
-} catch (Exception $e) {
-  var_dump($e); //錯誤內容
-}
 
+$body = file_get_contents("php://input");
+$events = $bot->parseEventRequest($body, $signature);
 foreach ($events as $event) {
         $reply_token = $event->getReplyToken();
         $text = $event->getText();
