ac0b0af58a57dd3d8677115359d178490f460629
Anavach Purivaro <aun.puri@gmail.com>
2017-03-01 15:28:56 +0700

    as

--- puridev.php
+++ puridev.php
@@ -3,7 +3,7 @@
 define("LINE_MESSAGING_API_CHANNEL_SECRET", '59cc1269e2956fd52b9c0eaadc70225c');
 define("LINE_MESSAGING_API_CHANNEL_TOKEN", '6tS7pO00ncfJFML6WrMEMXhtYru4rMFRapvH4qzPbxFp/2cf9dK6uxzzotYxyNMV51zGDZ23dznOogrpAhxNh3z881mOnyZ5M5mZVZPsyDj52DEvuJQZCf1u67UBBgkj+zrgPiD6n8Pd+lByPRTN0wdB04t89/1O/w1cDnyilFU=');
 
-require __DIR__."vendor/autoload.php";
+require __DIR__."/vendor/autoload.php";
 
 $bot = new \LINE\LINEBot(
     new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
