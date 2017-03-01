5330746f17eff7141c71f217f5e0fc5974a97947
Anavach Purivaro <aun.puri@gmail.com>
2017-03-01 22:06:07 +0700

    s

--- add_lineid.php
+++ add_lineid.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>โปรแกรมส่งข้อความ Line bot</title>
+    <title>โปรแกรมเพิ่ม Contact Line bot</title>
     <meta charset="utf-8">
     <meta http-equiv="Content-Type">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
