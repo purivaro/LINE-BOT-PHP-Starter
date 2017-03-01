60ab98dcdad39e5dcdd4a637ff0ac98abb678a29
Anavach Purivaro <aun.puri@gmail.com>
2017-03-01 17:49:57 +0700

    usห

--- puri_pushmsg.php
+++ puri_pushmsg.php
@@ -3,6 +3,8 @@ define("LINE_MESSAGING_API_CHANNEL_SECRET", '59cc1269e2956fd52b9c0eaadc70225c');
 define("LINE_MESSAGING_API_CHANNEL_TOKEN", '6tS7pO00ncfJFML6WrMEMXhtYru4rMFRapvH4qzPbxFp/2cf9dK6uxzzotYxyNMV51zGDZ23dznOogrpAhxNh3z881mOnyZ5M5mZVZPsyDj52DEvuJQZCf1u67UBBgkj+zrgPiD6n8Pd+lByPRTN0wdB04t89/1O/w1cDnyilFU=');
 
 require __DIR__."/vendor/autoload.php";
+$user_id = $_REQUEST['user_id'];
+// user id ของ puri = 'U02a2cb394330d90571a21b09f2c230ea'
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]);
@@ -10,7 +12,7 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => LINE_MESSAGING_API_CHA
 $text_send = "สวัสดี Puri";
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text_send);
-$response = $bot->pushMessage('U02a2cb394330d90571a21b09f2c230ea', $textMessageBuilder);
+$response = $bot->pushMessage($user_id, $textMessageBuilder);
 
 
 echo "$text_send OK";
