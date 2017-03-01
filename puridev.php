e4b024c1a7ef6c1f35d51554b8e59f2221b87939
Anavach Purivaro <aun.puri@gmail.com>
2017-03-01 23:16:24 +0700

    as

--- puridev.php
+++ puridev.php
@@ -47,13 +47,15 @@ foreach ($events as $event) {
         $_msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ขอบคุณที่ลงทะเบียน คุณ".$profile['displayName']);
         $messages->add($_msg);     
 
-        $_msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ID : $userId");
-        $messages->add($_msg);
 
         $_msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("$statusMessage");
         $messages->add($_msg);      
 
 /* 
+
+        $_msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ID : $userId");
+        $messages->add($_msg);
+
 
         $txt = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("$text");
         $messages->add($txt);    
