6768f32114c8dca7030b782249639886234c29ed
Anavach Purivaro <aun.puri@gmail.com>
2017-03-01 16:27:33 +0700

    s

--- puridev.php
+++ puridev.php
@@ -38,7 +38,7 @@ if(!is_null($events['events'])){
             $messages->add($imageMessageBuilder);
 
 
-            $response = $bot->replyMessage($replyToken, $imageMessageBuilder);
+            $response = $bot->replyMessage($replyToken, $messages);
 
             echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
