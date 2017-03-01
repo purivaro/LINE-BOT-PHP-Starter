a11c459be0ba978745efd90f3eb177a92cfb1074
Anavach Purivaro <aun.puri@gmail.com>
2017-03-01 16:36:47 +0700

    s

--- puridev.php
+++ puridev.php
@@ -14,11 +14,10 @@ $body = file_get_contents("php://input");
 $events = $bot->parseEventRequest($body, $signature);
 
 foreach ($events as $event) {
-    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
         $reply_token = $event->getReplyToken();
         $text = $event->getText();
         $bot->replyText($reply_token, $text);
-    }
+
 }
 echo "OK";
 ?>
