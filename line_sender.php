c67472eebcfc590bbb5789739535e45b0d0f4d4d
Anavach Purivaro <aun.puri@gmail.com>
2017-03-01 18:47:13 +0700

    a

---
+++ line_sender.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <tite>โปรแกรมส่งข้อความ Line bot</title>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link href="bower_components/bootstrap-material-design/dist/bootstrap-material-design.min.css">
+</head>
+<body>
+
+
+
+
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/boostrap.min.js"></script>
+    <script src="bower_components/bootstrap-material-design/dist/bootstrap-material-design.umd.js"></script>
+    <script>
+    $(document).ready(function(){
+        $.material.init();
+    });
+    </script>
+</body>
+</html>
