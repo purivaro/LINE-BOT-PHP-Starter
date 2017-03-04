8d6da3fad31d5219e584db5de97635c5ff8c202d
Anavach Purivaro <aun.puri@gmail.com>
2017-03-04 18:09:09 +0700

    a

---
+++ test2.php
@@ -0,0 +1,27 @@
+<?php
+/*
+$url_ibs = "http://www.ibsone.com/project/linebot/puribot/api/translate.php";
+$text = 'ลิง';
+$lang = 'สเปน';
+
+$data_ibs = [
+'text'=> $text,
+'lang'=>$lang
+];
+
+$post_ibs = json_encode($data_ibs);
+$headers_ibs = ['Content-Type: application/json'];
+$ch_ibs = curl_init($url_ibs);
+curl_setopt($ch_ibs, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch_ibs, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch_ibs, CURLOPT_POSTFIELDS, $post_ibs);
+curl_setopt($ch_ibs, CURLOPT_HTTPHEADER, $headers_ibs);
+curl_setopt($ch_ibs, CURLOPT_FOLLOWLOCATION, 1);
+$result_ibs = curl_exec($ch_ibs);
+curl_close($ch_ibs);
+
+$res = json_decode($result_ibs,true);
+$result = $res['result'];
+echo $result;
+*/
+?>
