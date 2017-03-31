0f7fff7fcd5083f55bc657ed9c99a4a9d74a87a4
พระอนวัช ภูริวโร <panavach@dhammakaya.center>
2017-03-31 22:46:19 +0700

    a

---
+++ dhammachak_reply.php
@@ -0,0 +1,48 @@
+<?php
+define("LINE_MESSAGING_API_CHANNEL_SECRET", '	
+cf0ce886078d72cb4dac84067938cd1a');
+define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'koGlCXsvgSe+hGyZLr+5ggRf9+hy+YUFjzuZIy19/dk2T7yJLkrSB/I+R4Qjipaym4/QlQI20kGjzag554KDZc596JWaSgp5juQALxAZChLyfFiZcJxZsbU/8iRcytkDq02Q6gkxKLCyPB7g9lnIvAdB04t89/1O/w1cDnyilFU=');
+
+require __DIR__."/vendor/autoload.php";
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]);
+
+
+$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$body = file_get_contents("php://input");
+try {
+  $events = $bot->parseEventRequest($body, $signature);
+} catch (Exception $e) {
+  var_dump($e); 
+}
+
+foreach ($events as $event) {
+        $reply_token = $event->getReplyToken();
+        $text = $event->getText();
+        $userId = $event->getUserId();
+        $type = $event->getType();
+
+		// https://line.github.io/line-bot-sdk-php/class-LINE.LINEBot.Event.BaseEvent.html#_getUserId
+        $messages = new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
+        for($i=0;$i<2;$i++)
+        {
+            $_msg = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("$userId".$i);
+            $messages->add($_msg);
+        }
+        $txt = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("$text");
+        $messages->add($txt);    
+        $txt = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("$type");
+        $messages->add($txt);    
+
+        $imageMessageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder("https://s-media-cache-ak0.pinimg.com/originals/3d/19/e2/3d19e22f8fc92cdbd53337558220e262.jpg","https://s-media-cache-ak0.pinimg.com/originals/3d/19/e2/3d19e22f8fc92cdbd53337558220e262.jpg");            
+        $messages->add($imageMessageBuilder);
+
+
+        $response = $bot->replyMessage($reply_token, $messages);
+
+        echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+}
+echo "OK";
+?>
