34e898c2282052f92208ac871a633f3a0934e914
Phra Anavach Purivaro <purivaro@Puris-MacBook-Pro.local>
2017-05-10 21:46:28 +0700

    a

--- puridev.php
+++ puridev.php
@@ -21,7 +21,8 @@ $database = $firebase->getDatabase();
 $error_log = $database->getReference('line/error_log');
 $error_log->push([
 		'error' => $e,
-		'test' => '555'
+		'test' => '555',
+		'event' => $events
 ]);
 
 
